58dd9b28f8d00aeb0628ccfe35d16c5882b74894
Bartłomiej Kuras <bartlomiej.kuras@o2.pl>
2019-12-01 09:54:00 +0100

    Day 1 excercise 1 done

--- day1-1/src/main.rs
+++ day1-1/src/main.rs
@@ -1,3 +1,11 @@
+use std::io::{stdin, BufRead, BufReader};
 fn main() {
-    println!("Hello, world!");
+    let result: i64 = BufReader::new(stdin())
+        .lines()
+        .filter_map(Result::ok)
+        .filter_map(|l| l.parse::<i64>().ok())
+        .map(|l| l / 3 - 2)
+        .sum();
+
+    println!("{}", result)
 }
