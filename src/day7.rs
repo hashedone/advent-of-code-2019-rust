0365414fe3b242b22bc679041532a6ce8d2aaed3
Bartłomiej Kuras <bartlomiej.kuras@o2.pl>
2019-12-07 23:38:18 +0100

    Small cleanup

--- src/day7.rs
+++ src/day7.rs
@@ -5,6 +5,7 @@ use async_std::prelude::*;
 use async_std::stream::{self, Stream};
 use async_std::sync::channel;
 use futures::future::join_all;
+use futures::stream::StreamExt;
 use permute::permutations_of;
 
 use crate::intcode::interpret;
@@ -27,12 +28,12 @@ pub async fn simplified<S: Stream<Item = String> + Unpin>(mut input: S) -> i64 {
     let results = permutations_of(&[0, 1, 2, 3, 4]).map(|phases| {
         let init: Pin<Box<dyn Stream<Item = i64>>> = Box::pin(stream::once(0));
 
-        phases
-            .fold(init, |input, phase| {
-                let input = stream::once(*phase).chain(input);
-                Box::pin(interpret(program.clone(), input))
-            })
-            .last()
+        let mut output = phases.fold(init, |input, phase| {
+            let input = stream::once(*phase).chain(input);
+            Box::pin(interpret(program.clone(), input))
+        });
+
+        async move { output.next().await }
     });
 
     join_all(results)
@@ -49,25 +50,23 @@ pub async fn extended<S: Stream<Item = String> + Unpin>(mut input: S) -> i64 {
 
     let results = permutations_of(&[5, 6, 7, 8, 9]).map(|phases| {
         let (send, recv) = channel(1);
-        let init: Pin<Box<dyn Stream<Item = i64>>> = Box::pin(stream::once(0).chain(recv));
+        let init: Pin<Box<dyn Stream<Item = _>>> = Box::pin(stream::once(0).chain(recv));
         let program = &program;
 
-        let init_fut: Pin<Box<dyn Future<Output = i64>>> = Box::pin(ready(0));
+        let init_fut: Pin<Box<dyn Future<Output = _>>> = Box::pin(ready(0));
         let mut last_outputs = phases.fold(init, move |input, phase| {
             let input = stream::once(*phase).chain(input);
             Box::pin(interpret(program.clone(), input))
         });
 
-        async move {
-            let mut result = 0;
-            while let Some(res) = last_outputs.next().await {
-                result = res;
-                send.send(res).await
+        last_outputs.fold(0, move |_, output| {
+            let send = send.clone();
+            async move {
+                send.clone().send(output).await;
+                output
             }
-            result
-        }
+        })
     });
 
     join_all(results).await.into_iter().max().unwrap()
 }
-
